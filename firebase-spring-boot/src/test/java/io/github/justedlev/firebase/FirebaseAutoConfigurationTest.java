f6e4603d6033e0753518f4e5d411c3b18390be0f
Edward L. <edward.lukashevits@gmail.com>
2026-04-14 20:41:27 +0300

    test: add more tests
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- firebase-spring-boot/src/test/java/io/github/justedlev/firebase/FirebaseAutoConfigurationTest.java
+++ firebase-spring-boot/src/test/java/io/github/justedlev/firebase/FirebaseAutoConfigurationTest.java
@@ -1,6 +1,9 @@
 package io.github.justedlev.firebase;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 import io.github.justedlev.firebase.autoconfigure.FirebaseAutoConfiguration;
 import io.github.justedlev.firebase.config.FirebaseConfigurationProperties;
 import io.github.justedlev.firebase.config.FirebaseTestConfiguration;
@@ -60,6 +63,74 @@ class FirebaseAutoConfigurationTest {
                 .run(ctx -> {
                     assertThat(ctx).doesNotHaveBean(FirebaseConfigurationProperties.class);
                     assertThat(ctx).doesNotHaveBean(FirebaseApp.class);
+                });
+    }
+
+    @Test
+    void whenAllEnabled_thenBeansCreated() {
+        new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(FirebaseAutoConfiguration.class))
+                .withUserConfiguration(FirebaseTestConfiguration.class)
+                .withPropertyValues(
+                        "firebase.enabled=true",
+                        "firebase.apps.default.project-id=default-project",
+                        "firebase.apps.default.database-url=http://localhost:0",
+                        "firebase.apps.default.auth.enabled=true",
+                        "firebase.apps.default.messaging.enabled=true"
+                )
+                .run(context -> {
+                    assertThat(context).hasSingleBean(FirebaseApp.class);
+                    assertThat(context).hasSingleBean(FirebaseDatabase.class);
+                    assertThat(context).hasSingleBean(FirebaseAuth.class);
+                    assertThat(context).hasSingleBean(FirebaseMessaging.class);
+                });
+    }
+
+    @Test
+    void whenDatabaseUrlIsSet_thenFirebaseDatabaseBeanCreated() {
+        new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(FirebaseAutoConfiguration.class))
+                .withUserConfiguration(FirebaseTestConfiguration.class)
+                .withPropertyValues(
+                        "firebase.enabled=true",
+                        "firebase.apps.default.project-id=default-project",
+                        "firebase.apps.default.database-url=http://localhost:0"
+                )
+                .run(context -> {
+                    assertThat(context).hasSingleBean(FirebaseApp.class);
+                    assertThat(context).hasSingleBean(FirebaseDatabase.class);
+                });
+    }
+
+    @Test
+    void whenAuthEnabled_thenFirebaseAuthBeanCreated() {
+        new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(FirebaseAutoConfiguration.class))
+                .withUserConfiguration(FirebaseTestConfiguration.class)
+                .withPropertyValues(
+                        "firebase.enabled=true",
+                        "firebase.apps.default.project-id=default-project",
+                        "firebase.apps.default.auth.enabled=true"
+                )
+                .run(context -> {
+                    assertThat(context).hasSingleBean(FirebaseApp.class);
+                    assertThat(context).hasSingleBean(FirebaseAuth.class);
+                });
+    }
+
+    @Test
+    void whenMessagingEnabled_thenFirebaseMessagingBeanCreated() {
+        new ApplicationContextRunner()
+                .withConfiguration(AutoConfigurations.of(FirebaseAutoConfiguration.class))
+                .withUserConfiguration(FirebaseTestConfiguration.class)
+                .withPropertyValues(
+                        "firebase.enabled=true",
+                        "firebase.apps.default.project-id=default-project",
+                        "firebase.apps.default.messaging.enabled=true"
+                )
+                .run(context -> {
+                    assertThat(context).hasSingleBean(FirebaseApp.class);
+                    assertThat(context).hasSingleBean(FirebaseMessaging.class);
                 });
     }
 }
