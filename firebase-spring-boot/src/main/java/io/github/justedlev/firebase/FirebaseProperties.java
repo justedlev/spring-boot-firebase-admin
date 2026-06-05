8e048b1f44c09d019672bace4fd3fbd9c82f8bca
Edward L. <edward.lukashevits@gmail.com>
2026-06-05 11:37:41 +0300

    refactor: add `@lombok.Builder.Default` on fields
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- firebase-spring-boot/src/main/java/io/github/justedlev/firebase/FirebaseProperties.java
+++ firebase-spring-boot/src/main/java/io/github/justedlev/firebase/FirebaseProperties.java
@@ -27,9 +27,14 @@ public class FirebaseProperties {
     private Integer connectTimeout;
     private Integer readTimeout;
     private Integer writeTimeout;
+    @lombok.Builder.Default
     private FirebaseAuthProperties auth = new FirebaseAuthProperties();
+    @lombok.Builder.Default
     private FirebaseDatabaseProperties db = new FirebaseDatabaseProperties();
+    @lombok.Builder.Default
     private FirebaseMessagingProperties messaging = new FirebaseMessagingProperties();
+    @lombok.Builder.Default
     private StorageProperties storage = new StorageProperties();
+    @lombok.Builder.Default
     private FirestoreProperties firestore = new FirestoreProperties();
 }
