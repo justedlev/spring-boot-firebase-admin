dc95355560a41320d19a2aa0bc2e585e381b05cb
Edward L. <edward.lukashevits@gmail.com>
2026-06-09 21:39:31 +0300

    feat: optimize for spring boot v2
    Release-As: 1.0.0
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- firebase-spring-boot/src/main/java/io/github/justedlev/firebase/config/FirebaseAppsBeanDefinitionRegistryPostProcessor.java
+++ firebase-spring-boot/src/main/java/io/github/justedlev/firebase/config/FirebaseAppsBeanDefinitionRegistryPostProcessor.java
@@ -5,14 +5,16 @@ import io.github.justedlev.firebase.FirebaseOptionsFactory;
 import io.github.justedlev.firebase.FirebaseProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -56,5 +58,10 @@ public class FirebaseAppsBeanDefinitionRegistryPostProcessor implements BeanDefi
 
     private Supplier<FirebaseApp> firebaseAppSupplier(FirebaseProperties props) {
         return () -> FirebaseApp.initializeApp(optionsFactory.create(props), props.getName());
+    }
+
+    @Override
+    public void postProcessBeanFactory(@Nullable ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        // nothing to do
     }
 }
